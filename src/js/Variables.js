0f8493dac5a6b37bc5ce5e396a69053b146b9e38
Laurens Rinke <mail@laurens-rinke.de>
2014-01-31 16:53:05 +0100

    added Previev Graph switching and workspace change if click on stage in setup mode

--- src/js/Variables.js
+++
@@ -1,8 +0,0 @@
-var Variables = new Object();
-Variables = {
-	varInteger:["myInteger","testInt","potiPin"],
-	varFloat:["myFloat","testFloat"],
-	varByte:["ardByte","testByte"],
-	varBoolean:["toggleMenue","listSomething"],
-	varString:["myNameIs","text"]
-};
