9c292752f22e0fd3a5fdbd38095760c92138139e
Laurens Rinke <mail@laurens-rinke.de>
2014-01-31 17:01:02 +0100

    refilled repo with latest build

---
+++ src/js/Variables.js
@@ -0,0 +1,8 @@
+var Variables = new Object();
+Variables = {
+	varInteger:["myInteger","testInt","potiPin"],
+	varFloat:["myFloat","testFloat"],
+	varByte:["ardByte","testByte"],
+	varBoolean:["toggleMenue","listSomething"],
+	varString:["myNameIs","text"]
+};
---
+++ src/js/globalFunctions.js
@@ -0,0 +1,133 @@
+function generateUUID() {
+
+	
+		// http://www.ietf.org/rfc/rfc4122.txt
+		var s = [];
+		var hexDigits = "0123456789abcdef";
+		for (var i = 0; i < 36; i++) {
+			s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+		}
+		s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+		s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+		s[8] = s[13] = s[18] = s[23] = "-";
+		
+		var uuid = s.join("");
+		return uuid;
+
+	}
+
+
+function disableDblClick(elementName) {
+	$(elementName).dblclick(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Graph Object
+
+function setActiveGraph(that){
+	activeGraph.id = $(that).attr('id');
+	activeGraph.pInstance = Processing.getInstanceById(activeGraph.id);
+	activeGraph.X = that.pageX;
+	activeGraph.Y = that.pageY;
+	changeActiveGraph();
+
+	
+
+}
+
+function resetActiveGraph(){
+	activeGraph = new Object();
+	changeActiveGraph();
+}
+
+
+function changeActiveGraph() {
+	if (activeGraph.id) {
+		$('.GraphCanvas').addClass('passiv');
+		var id = '#'+activeGraph.id;
+		$(id).removeClass('passiv');
+		
+		
+	}else{
+		$('.GraphCanvas').removeClass('passiv');
+	}
+	
+	
+	
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Mode Select
+
+function setMode (mode) {
+
+
+	if (typeof mode == "object") {
+		var that = mode;
+		$('#ModeSelect a').removeClass('activ');
+		$(that).addClass('activ');
+	}
+
+	if (typeof mode == "string") {
+	switch (mode){
+		case "work":
+		$('#ModeSelect a').removeClass('activ');
+		$('#ModeSelect li:first-child a').addClass('activ');
+		break;
+
+		case "setup":
+		$('#ModeSelect a').removeClass('activ');
+		$('#ModeSelect li:last-child a').addClass('activ');
+		break;
+	}
+	}
+	if(ModeRead()==='setup'){
+		
+		$('.UIWindow').tabs({'active':2});
+		$('.GraphCanvas').addClass('passiv');
+		$('#stage').bind("click",function () {
+			setMode("work");
+		});
+		$('.GraphCanvas').draggable({ disabled: false });
+
+
+	if (activeGraph.id) {
+		$('.GraphCanvas').addClass('passiv');
+		var id = '#'+activeGraph.id;
+		$(id).removeClass('passiv');
+	}
+
+	}else{
+		$('.UIWindow').tabs({'active':0});
+		$('.GraphCanvas').removeClass('passiv');
+		resetActiveGraph();
+		$('#stage').bind("click",function () {
+			resetActiveGraph();
+		});
+		$('.GraphCanvas').draggable({ disabled: true });
+	}
+}
+
+function ModeRead () {
+	return $('#ModeSelect .activ').attr('title');
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Print to Console
+
+function say (argument) {
+	console.log(argument);
+}
+
+
+function animate() {
+	this.rValue = Math.random()*1024;
+
+}
+
+var run = setInterval(animate,10);
